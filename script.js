45528913eb94bd84e86651b6e0856ac71efc1304
Clinton Johnson <dragoon1246224@gmail.com>
2018-09-27 10:56:26 -0400

    Finished

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-class grid (height, width) 
+class Grid {
+    constructor(options) {
+        this.gridArray = []
+        this.numberOfRows = options.numberOfRows || 5
+        this.numberOfColumns = options.numberOfColumns || 5
+        this.gridContainer = document.getElementById("main")
+        this.elementId = options.elementId
+        this.newRows()
+
+    }
+
+
+    newRows() {
+        for (let rowIndex = 0; rowIndex < this.numberOfRows; rowIndex++) {
+            this.rowIndex = rowIndex
+            this.rowArray = []
+            this.gridArray.push(this.rowArray)
+            this.rowElement = document.createElement("div")
+            this.rowElement.classList.add("newDiv")
+            this.gridContainer.appendChild(this.rowElement)
+            for (let columnIndex = 0; columnIndex < this.numberOfColumns; columnIndex++) {
+                const cell = new Cell(rowIndex, columnIndex, this.rowElement)
+                this.rowArray.push(cell)
+            }
+        }
+    }
+}
+
+class Cell {
+    constructor(rowIndex, columnIndex, rowParent) {
+        this.rowIndex = rowIndex
+        this.columnIndex = columnIndex
+        this.rowParent = rowParent
+        this.createCell()
+    }
+    createCell() {
+        this.cell = document.createElement("span")
+        this.rowParent.appendChild(this.cell)
+    }
+}
+
+const grid = new Grid({
+    numberOfRows: 5,
+    numberOfColumns: 5,
+    parentContainerId: "main",
+})
